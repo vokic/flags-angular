ff57877db7b08b153e7df19989e897557aee5e5f
viktor <viktor011@gmail.com>
2024-04-19 00:30:49 +0200

    various changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { Component, EventEmitter, Output } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatTooltipModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
@@ -17,5 +18,11 @@ export class NavbarComponent {
   toggleDarkTheme() {
     this.darkThemeEnabled = !this.darkThemeEnabled;
     this.darkThemeToggled.emit(this.darkThemeEnabled);
+
+    // Toggle a class on the body element
+    const body = document.querySelector('body');
+    if (body) {
+      body.classList.toggle('dark-theme', this.darkThemeEnabled);
+    }
   }
 }
